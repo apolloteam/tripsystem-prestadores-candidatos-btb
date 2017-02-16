73b16240193efdfeee76d3c95a1211741b920ba6
Rinaldo Julio Sassaroli <nanosassa@gmail.com>
2017-02-16 16:22:24 -0300

    Added Post to API Feature

---
+++ ingresochoferes.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+ 
+  $('.container > form').on('submit', function (e) {
+      
+      e.preventDefault();
+
+      postToAPI(
+          function (result) { // Success Call
+            $('.enviadoconductores > h3').fadeIn();
+          },
+          function (error) {  // Error Call
+            // TODO: Show Error Message
+            console.log(arguments)
+            var response = JSON.parse(error.responseText);
+            $('.enviadoconductores > h3').html('error').fadeIn();
+          }
+        );
+  })
+  
+})
+        
+
+function postToAPI(postSuccess, postError) {
+
+  var requestData = {
+      nombres :  $('#nombrechofer').val(),
+      apellido : $('#apellidochofer').val(),
+      email : $('#mailchofer').val(),
+      telMovil : $('#celularchofer').val()
+  };
+  
+  console.log(requestData); //use the console for debugging, F12 in Chrome, not alerts
+
+
+  //Post to API
+
+  var requestDataJSON = JSON.stringify(requestData);
+  var APIurl = 'http://api.traslada.com.ar/prestadores/candidatos'; // (Live)
+
+  $.ajax({
+    type: 'POST',
+    url: APIurl,
+    contentType: 'application/json',
+    data: requestDataJSON,
+    success: postSuccess,
+    error: postError,
+    dataType: 'json',
+    crossDomain: true
+  });
+
+}
+
